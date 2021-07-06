16db04795a86c9e478ce9405d6f6d71ca682cd9d
Cam <cam.ball96@hotmail.com>
2021-07-06 16:17:10 +1000

    Updated bugs where save button was not saving and background colours werent correlating to time of day

--- script.js
+++ script.js
@@ -13,23 +13,25 @@ $(document).ready(function () {
     })
 
 
-    // Collecting saved data from local storage
-    $("#9am .textArea").val(localStorage.getItem("9:00"));
-    $("#10am .textArea").val(localStorage.getItem("10:00"));
-    $("#11am .textArea").val(localStorage.getItem("11:00"));
-    $("#12pm .textArea").val(localStorage.getItem("12:00"));
-    $("#1pm .textArea").val(localStorage.getItem("13:00"));
-    $("#2pm .textArea").val(localStorage.getItem("14:00"));
-    $("#3pm .textArea").val(localStorage.getItem("15:00"));
-    $("#4pm .textArea").val(localStorage.getItem("16:00"));
-    $("#5pm .textArea").val(localStorage.getItem("17:00"));
-
-
     //clears all local storage data
-        $("#clearBtn").on("click", function(){
-            localStorage.clear();
-            initPage()
-        }) 
+    $("#clearBtn").on("click", function(){
+        localStorage.clear();
+        location.reload();
+    })
+
+    // Collecting saved data from local storage
+    $("#9 .textArea").val(localStorage.getItem("9"));
+    $("#10 .textArea").val(localStorage.getItem("10"));
+    $("#11 .textArea").val(localStorage.getItem("11"));
+    $("#12 .textArea").val(localStorage.getItem("12"));
+    $("#13 .textArea").val(localStorage.getItem("13"));
+    $("#14 .textArea").val(localStorage.getItem("14"));
+    $("#15 .textArea").val(localStorage.getItem("15"));
+    $("#16 .textArea").val(localStorage.getItem("16"));
+    $("#17 .textArea").val(localStorage.getItem("17"));
+
+
+    
 
 
     // Function that changes text area background in correlation to current time.
